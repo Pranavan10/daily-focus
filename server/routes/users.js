7210e2e3e7a3932dd8a9e73a67c4e52426543649
Pranavan <pranavan10@outlook.com>
2021-03-18 17:26:50 +1300

    fixed console.error and removed validator

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,6 +1,5 @@
 var express = require("express");
 var usersRouter = express.Router();
-const emptyLogin = require("./../validators");
 const firebase = require("firebase");
 
 /* GET users listing. */
@@ -14,9 +13,7 @@ function login(req, res) {
         email: req.body.email,
         password: req.body.password,
     };
-    //Validity check for login inputs
-    const { valid, errors } = emptyLogin(user);
-    if (!valid) return res.status(400).json(errors);
+
     //checks if matching email and password exist on firebase servers
     firebase
         .auth()
@@ -28,8 +25,8 @@ function login(req, res) {
             return res.json({ token });
         })
         .catch((err) => {
-            console.err(err);
-            return res.status(403).json({ Message: "Either your email or password is incorrect" });
+            console.error(err);
+            return res.status(401).json({ Message: "Either your email or password is incorrect" });
         });
 }
 
