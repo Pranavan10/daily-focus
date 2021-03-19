ca46a6505205a56419aaa48510392668b6fdf73e
Pranavan <pranavan10@outlook.com>
2021-03-19 13:59:19 +1300

    removed test endpoint and unecessary info

--- server/app.js
+++ server/app.js
@@ -36,7 +36,7 @@ app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render("error");
 });
-// app.post("/auth", authorise);
+
 //endpoint for login
 app.post("/login", authorise, login);
 
--- server/auth.js
+++ server/auth.js
@@ -1,13 +1,6 @@
 var admin = require("firebase-admin");
 admin.initializeApp({
-    apiKey: "AIzaSyAZ3IXyz6_ALJfUaQRQGRVT-szNGHTexbg",
-    authDomain: "daily-focus-a7423.firebaseapp.com",
-    databaseURL: "https://daily-focus-a7423-default-rtdb.firebaseio.com",
     projectId: "daily-focus-a7423",
-    storageBucket: "daily-focus-a7423.appspot.com",
-    messagingSenderId: "30654869758",
-    appId: "1:30654869758:web:d4041638ea4bd76c8a35af",
-    measurementId: "G-HFNWF4DM71",
 });
 function authorise(request, response, next) {
     let idToken;
