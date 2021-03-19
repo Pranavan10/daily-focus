11b6683891d36fee1eaf1ec34a819618efce2f33
Pranavan <pranavan10@outlook.com>
2021-03-19 14:21:59 +1300

    changed var named

--- server/auth.js
+++ server/auth.js
@@ -2,23 +2,23 @@ var admin = require("firebase-admin");
 admin.initializeApp({
     projectId: "daily-focus-a7423",
 });
-function authorise(request, response, next) {
+function authorise(res, res, next) {
     let idToken;
-    if (request.headers.authorization && request.headers.authorization.startsWith("Bearer ")) {
-        idToken = request.headers.authorization.split("Bearer ")[1];
+    if (res.headers.authorization && res.headers.authorization.startsWith("Bearer ")) {
+        idToken = res.headers.authorization.split("Bearer ")[1];
     } else {
         console.error("No bearer token found");
-        return response.status(401).json({ error: "Unauthorized access" });
+        return res.status(401).json({ error: "Unauthorized access" });
     }
     admin
         .auth()
         .verifyIdToken(idToken)
-        .then((Token) => {
+        .then((token) => {
             return next();
         })
         .catch((err) => {
             console.error("Error while attempting to verifying token", err);
-            return response.status(401).json(err);
+            return res.status(401).json(err);
         });
 }
 module.exports = authorise;
